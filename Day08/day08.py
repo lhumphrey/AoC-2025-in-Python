8775948ca8f3f8776d6d9b756c393f0583d6b6cd
Laura Humphrey <laura.r.humphrey@nasa.gov>
2025-12-08 23:07:59 -0500

    Day08

--- Day08/day08.py
+++ Day08/day08.py
@@ -7,14 +7,13 @@ def distance(x1, x2):
 
 def box_is_in_circuit(box, circuits):
     for i in range(len(circuits)):
-        for circuit_box_pair in circuits[i]:
-            if box == circuit_box_pair[0] or box == circuit_box_pair[1]:
-                return i
+        if box in circuits[i]:
+            return i
     return -1
 
 
 # Main logic
-with open('input_ex.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     lines = [line.rstrip('\n') for line in file]
 
 box_locs = []
@@ -26,34 +25,71 @@ for i in range(len(box_locs)):
     for j in range(i + 1,len(box_locs)):
         box_pairs.add([box_locs[i], box_locs[j]])
 
-circuits = SortedKeyList(key=lambda x: len(x))
-circuits.add([box_pairs[0]])
-for i in range(1,10):
+circuits = SortedKeyList(key=lambda x: -len(x))
+circuits.add([box_pairs[0][0], box_pairs[0][1]])
+for i in range(1,1000):
     box_pair = box_pairs[i]
-    ind1 = box_is_in_circuit(box_pair[0], circuits)
-    ind2 = box_is_in_circuit(box_pair[1], circuits)
-    if ind1 < 0 and ind2 < 0:
-        circuits.add([box_pair])
-    elif ind1 >= 0 and ind2 < 0:
-        circuits[ind1].append(box_pair)
-    elif ind1 < 0 and ind2 >= 0:
-        circuits[ind2].append(box_pair)
-    elif ind1 != ind2:
-        circuit_1 = circuits[ind1]
-        circuit_2 = circuits[ind2]
-        for circuit_2_box_pair in circuit_2:
-            circuit_1.append(circuit_2_box_pair)
-        circuits.pop(ind2)
-        circuit_1.append(box_pair)
-
-#for c in circuits:
-#    print(c)
-# 135 is too low
+    ind0 = box_is_in_circuit(box_pair[0], circuits)
+    ind1 = box_is_in_circuit(box_pair[1], circuits)
+    if ind0 < 0 and ind1 < 0:
+        circuits.add([box_pair[0], box_pair[1]])
+    elif ind0 >= 0 and ind1 < 0:
+        tmp = circuits[ind0]
+        circuits.pop(ind0)
+        tmp.append(box_pair[1])
+        circuits.add(tmp)
+    elif ind0 < 0 and ind1 >= 0:
+        tmp = circuits[ind1]
+        circuits.pop(ind1)
+        tmp.append(box_pair[0])
+        circuits.add(tmp)
+    elif ind0 != ind1:
+        tmp0 = circuits[ind0]
+        tmp1 = circuits[ind1]
+        for second_circuit_box in tmp1:
+            tmp0.append(second_circuit_box)
+        if ind0 > ind1:
+            circuits.pop(ind0)
+            circuits.pop(ind1)
+        else:
+            circuits.pop(ind1)
+            circuits.pop(ind0)
+        circuits.add(tmp0)
 
 count1 = 1
 for i in range(3):
-    count1 *= len(circuits[i]) + 1
+    count1 *= len(circuits[i])
 print('The total for part 1 is: ' + str(count1))
 
-count2 = 0
-print('The total for part 2 is: ' + str(count2))
+for i in range(1000, len(box_pairs) - 1):
+    box_pair = box_pairs[i]
+    ind0 = box_is_in_circuit(box_pair[0], circuits)
+    ind1 = box_is_in_circuit(box_pair[1], circuits)
+    if ind0 < 0 and ind1 < 0:
+        circuits.add([box_pair[0], box_pair[1]])
+    elif ind0 >= 0 and ind1 < 0:
+        tmp = circuits[ind0]
+        circuits.pop(ind0)
+        tmp.append(box_pair[1])
+        circuits.add(tmp)
+    elif ind0 < 0 and ind1 >= 0:
+        tmp = circuits[ind1]
+        circuits.pop(ind1)
+        tmp.append(box_pair[0])
+        circuits.add(tmp)
+    elif ind0 != ind1:
+        tmp0 = circuits[ind0]
+        tmp1 = circuits[ind1]
+        for second_circuit_box in tmp1:
+            tmp0.append(second_circuit_box)
+        if ind0 > ind1:
+            circuits.pop(ind0)
+            circuits.pop(ind1)
+        else:
+            circuits.pop(ind1)
+            circuits.pop(ind0)
+        circuits.add(tmp0)
+    if len(circuits) == 1 and len(circuits[0]) == len(box_locs):
+        break
+
+print('The total for part 2 is: ' + str(box_pair[0][0] * box_pair[1][0]))
