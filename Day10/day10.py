a9692946f6fc1a7230f8a158a7e171220a63af37
Laura Humphrey <laura.r.humphrey@nasa.gov>
2025-12-14 16:43:50 -0500

    Day10

--- Day10/day10.py
+++ Day10/day10.py
@@ -1,4 +1,6 @@
 import re
+import numpy as np
+import pulp
 
 def light_str_to_num(light_str):
     binary_str = ''
@@ -47,25 +49,28 @@ def find_solution_1(light_panel, buttons):
         counter += 1
 
 
-def find_solution_1(light_panel, buttons):
-    step = []
-    for button in buttons:
-        step.append(light_panel ^ button)
-        if light_panel ^ button == 0:
-            return 1
+def find_solution_2(button_vectors, joltages):
 
-    # This could be made more efficient. There's no reason to press
-    # the same button twice, but I'm being lazy.
-    counter = 2
-    while counter < 100:
-        step_tmp = []
-        for s in step:
-            for button in buttons:
-                step_tmp.append(s ^ button)
-                if step_tmp[-1] == 0:
-                    return counter
-        step = step_tmp
-        counter += 1
+    A = np.array([np.array(bv) for bv in button_vectors])
+    A = A.T
+    B = np.array(joltages)
+    prob = pulp.LpProblem("Integer_System", pulp.LpMinimize)
+    x = [pulp.LpVariable(f'x{i}', lowBound=0, cat=pulp.LpInteger) for i in range(A.shape[1])]
+    prob += pulp.lpSum(x)
+
+    # Add the equality constraints
+    for row_idx in range(A.shape[0]):
+        prob += (pulp.lpSum(A[row_idx, col_idx] * x[col_idx] for col_idx in range(A.shape[1])) == B[row_idx])
+
+    # Solve
+    status = prob.solve()
+    if pulp.LpStatus[status] == "Optimal":
+        x_values = [var.varValue for var in x]
+    else:
+        raise RuntimeError
+
+    return int(sum(x_values))
+
 
 # Main logic
 with open('input.txt', 'r') as file:
@@ -76,6 +81,7 @@ button_sets_1 = []
 button_sets_2 = []
 joltages = []
 count1 = 0
+count2 = 0
 for l in lines:
     m = re.search('[#.]+', l)
     light_str = l[m.start():m.end()]
@@ -85,15 +91,23 @@ for l in lines:
     for m in f:
         num_strs.append(m.group())
     button_list = []
-    button_pos_list = []
+    button_vec_list = []
     for n in num_strs[:-1]:
         button_pos = [int(b) for b in n.replace(',','')]
-        button_pos_list.append(button_pos)
         button_list.append(button_pos_list_to_num(button_pos, len(light_str)))
+        button_vec_tmp = [0] * len(light_str)
+        for bp in button_pos:
+            button_vec_tmp[bp] = 1
+        button_vec_list.append(button_vec_tmp)
     button_sets_1.append(button_list)
-    button_sets_2.append(button_pos_list)
     joltages.append([int(b) for b in num_strs[-1].split(',')])
-    num_pushes = find_solution_1(light_panels[-1], button_sets[-1])
+    button_sets_2.append(button_vec_list)
+
+    num_pushes = find_solution_1(light_panels[-1], button_sets_1[-1])
     count1 += num_pushes
+    num_pushes = find_solution_2(button_sets_2[-1], joltages[-1])
+    count2 += num_pushes
+    # print(str(num_pushes))
 
 print('The count for Part 1 is: ' + str(count1))
+print('The count for Part 2 is: ' + str(count2))
